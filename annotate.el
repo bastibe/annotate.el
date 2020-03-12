085791450c9fcc045bb1c226ac27f5ffaf752876
cage <cage-invalid@invalid>
2020-03-12 14:50:33 +0100

    - changed version number.

--- annotate.el
+++ annotate.el
@@ -7,7 +7,7 @@
 ;; Maintainer: Bastian Bechtold
 ;; URL: https://github.com/bastibe/annotate.el
 ;; Created: 2015-06-10
-;; Version: 0.5.4
+;; Version: 0.6.0
 
 ;; This file is NOT part of GNU Emacs.
 
@@ -55,7 +55,7 @@
 ;;;###autoload
 (defgroup annotate nil
   "Annotate files without changing them."
-  :version "0.5.4"
+  :version "0.6.0"
   :group 'text)
 
 ;;;###autoload
