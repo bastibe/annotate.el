754db391fd19da3e17d115b0feead2ea0134f872
cage <cage-invalid@invalid>
2020-06-24 11:45:16 +0200

    - updated section 'known bugs' of README.

--- annotate.el
+++ annotate.el
@@ -7,7 +7,7 @@
 ;; Maintainer: Bastian Bechtold
 ;; URL: https://github.com/bastibe/annotate.el
 ;; Created: 2015-06-10
-;; Version: 0.8.0
+;; Version: 0.8.1
 
 ;; This file is NOT part of GNU Emacs.
 
@@ -55,7 +55,7 @@
 ;;;###autoload
 (defgroup annotate nil
   "Annotate files without changing them."
-  :version "0.8.0"
+  :version "0.8.1"
   :group 'text)
 
 ;;;###autoload
