a02755056ef37ad9e3c75b915919fe96f2528ac3
cage <cage-invalid@invalid>
2020-07-31 18:29:23 +0200

    - changed occurrence of 'annotate-load-annotation-data' to 'annotate-load-annotation-data-ignore-errors'.
    The existing code relies on the fact that a non existing database file
    can  be  considered   as  an  empty  database,  wrote   a  wrapper  of
    'annotate-load-annotation-data' to keep the old behaviour.

--- annotate.el
+++ annotate.el
@@ -1095,7 +1095,7 @@ essentially what you get from:
                                           (= (annotate-beginning-of-annotation a)
                                              (annotate-ending-of-annotation    a)))
                                         (annotate-describe-annotations)))
-        (all-annotations  (annotate-load-annotation-data))
+        (all-annotations  (annotate-load-annotation-data-ignore-errors))
         (filename         (annotate-guess-filename-for-dump (annotate-actual-file-name))))
     (if (assoc-string filename all-annotations)
         (setcdr (assoc-string filename all-annotations)
@@ -1120,7 +1120,7 @@ essentially what you get from:
   "Load all annotations from disk in old format."
   (interactive)
   (let ((annotations (cdr (assoc-string (annotate-actual-file-name)
-                                        (annotate-load-annotation-data))))
+                                        (annotate-load-annotation-data-ignore-errors))))
         (modified-p  (buffer-modified-p)))
     ;; remove empty annotations created by earlier bug:
     (setq annotations (cl-remove-if (lambda (ann) (null (nth 2 ann)))
@@ -1181,7 +1181,7 @@ example:
                             (not (stringp (cl-first (last annotation))))))
     (interactive)
     (let* ((filename             (annotate-actual-file-name))
-           (all-annotations-data (annotate-load-annotation-data))
+           (all-annotations-data (annotate-load-annotation-data-ignore-errors))
            (annotation-dump      (assoc-string filename all-annotations-data))
            (annotations          (annotate-annotations-from-dump annotation-dump))
            (old-checksum         (annotate-checksum-from-dump annotation-dump))
@@ -1235,8 +1235,14 @@ i.e. the first record is removed."
  "Update database *on disk* removing all the records with empty
 annotation."
   (interactive)
-  (let ((db (annotate-db-clean-records (annotate-load-annotation-data))))
+  (let ((db (annotate-db-clean-records (annotate-load-annotation-data-ignore-errors))))
     (annotate-dump-annotation-data db)))
+
+(defun annotate-load-annotation-data-ignore-errors ()
+  (condition-case error-message
+      (annotate-load-annotation-data)
+    (annotate-db-file-not-found
+     nil)))
 
 (defun annotate-load-annotation-data ()
   "Read and return saved annotations."
@@ -1854,7 +1860,7 @@ sophisticated way than plain text"
          (ending          (button-get button 'ending))
          (begin-of-button (button-get button 'begin-of-button))
          (end-of-button   (button-get button 'end-of-button))
-         (db              (annotate-load-annotation-data))
+         (db              (annotate-load-annotation-data-ignore-errors))
          (filtered        (annotate-db-remove-annotation db filename beginning ending)))
     (annotate-dump-annotation-data filtered) ; save the new database with entry removed
     (cl-labels ((redraw-summary-window () ; update the summary window
@@ -1883,7 +1889,7 @@ sophisticated way than plain text"
          (annotation-beginning (button-get button 'beginning))
          (annotation-ending    (button-get button 'ending))
          (query                (button-get button 'query))
-         (db                   (annotate-load-annotation-data))
+         (db                   (annotate-load-annotation-data-ignore-errors))
          (old-annotation       (button-get button 'text))
          (new-annotation-text  (read-from-minibuffer annotate-annotation-prompt old-annotation)))
     (when (not (annotate-string-empty-p new-annotation-text))
@@ -2010,7 +2016,7 @@ results can be filtered with a simple query language: see
                               (t
                                ".*"))))
     (let* ((filter-query (get-query))
-           (dump         (annotate-summary-filter-db (annotate-load-annotation-data)
+           (dump         (annotate-summary-filter-db (annotate-load-annotation-data-ignore-errors)
                                                      filter-query)))
       (if (db-empty-p dump)
           (when annotate-use-messages
