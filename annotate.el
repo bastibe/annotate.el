aa3c0540ee644e0e205644545d659a6ba16d76ac
cage <cage@invalid>
2022-02-25 10:24:07 +0100

    - updated version number;
    - updated NEWS and Changelog.

--- annotate.el
+++ annotate.el
@@ -7,7 +7,7 @@
 ;; Maintainer: Bastian Bechtold
 ;; URL: https://github.com/bastibe/annotate.el
 ;; Created: 2015-06-10
-;; Version: 1.5.0
+;; Version: 1.5.1
 
 ;; This file is NOT part of GNU Emacs.
 
