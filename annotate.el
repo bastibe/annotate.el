866fa6d3c05baeace4e8906049a356c9ea913a23
cage <cage>
2023-10-10 15:47:36 +0200

    - increased version number.

--- annotate.el
+++ annotate.el
@@ -7,7 +7,7 @@
 ;; Maintainer: Bastian Bechtold <bastibe.dev@mailbox.org>, cage <cage-dev@twistfold.it>
 ;; URL: https://github.com/bastibe/annotate.el
 ;; Created: 2015-06-10
-;; Version: 2.0.3
+;; Version: 2.1.0
 
 ;; This file is NOT part of GNU Emacs.
 
@@ -58,7 +58,7 @@
 ;;;###autoload
 (defgroup annotate nil
   "Annotate files without changing them."
-  :version "2.0.3"
+  :version "2.1.0"
   :group 'text)
 
 (defvar annotate-mode-map
